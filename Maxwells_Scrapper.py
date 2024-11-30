f2e27cec105710b18a6d1e0dada6c3c582de7cc8
Maxwelloper14 <maxwellkiriechka@gmail.com>
2024-11-30 11:37:00 +0300

    Add files via upload
    The scrapper

---
+++ Maxwells_Scrapper.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+def scan_wifi_networks():
+    print("Сканирование ближайших Wi-Fi сетей...")
+    result = subprocess.run(['netsh', 'wlan', 'show', 'network'], capture_output=True, text=True)
+    networks = result.stdout
+    print(networks)
+
+def show_connected_devices():
+    print("Список подключенных устройств...")
+    result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
+    devices = result.stdout
+    print(devices)
+
+def main():
+    scan_wifi_networks()
+    show_connected_devices()
+
+if name == "main":
+    main()
